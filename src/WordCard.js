165b4d315d141457e1c7a270d5ff806d8bf5012c
wilawanhnoonkhong <areeyadaoh@gmail.com>
2018-09-13 12:06:08 +0700

    check3

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import CharacterCard from './CharacterCard';
+
 export default class WordCard extends
-Component {
-render() {
- return (
- <div>
-     <WordCard value = {word} />
- </div>
- );
- }
+    Component {
+        constructor{
+    super(props)
+    this.state = {
+        attempt : '',
+    }
 }
+activationHandler = c => { console.log(${c} has been activated.) }
+render() {
+    return (
+        <div>
+            {Array.from(this.props.value).map((c, i) =><CharacterCard value={c} key={i} attempt={this.state.attempt}
+            activationHandler={this.activationHandler}/>)}
+        </div>
+    );
+}
+    }
